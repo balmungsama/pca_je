a695e2a6dd8284f4603f0e060f7192249d5cdc2d
John Eusebio <jbryaneusebio@gmail.com>
2018-01-21 02:05:02 -0500

    added run() command

--- pca_fmri.sh
+++ pca_fmri.sh
@@ -3,4 +3,4 @@
 #SBATCH --mem=10240     # Memory requested in megabytes. If omitted, the default is 1024 MB.
 #SBATCH -t 0-20:0:0      # How long will your job run for? If omitted, the default is 3 hours.
 
-matlab -nodesktop -nosplash -r 
+matlab -nodesktop -nosplash -r 'run(/global/home/hpc3586/JE_packages/pca_je/pca_fmri.m)'
