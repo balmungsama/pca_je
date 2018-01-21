7e1d13a7b0354800a8de9bd80245d37e20842c32
John Eusebio <jbryaneusebio@gmail.com>
2018-01-21 02:12:15 -0500

    terminated function with 'end'

--- pca_fmri.m
+++ pca_fmri.m
@@ -103,22 +103,23 @@ save(output, 'pls_out');
 
 function [pc_ind, pc_sign] = sort_eigen_images(orig_V, bs_V)
 
-numpcs.orig = size(orig_V, 2);
-numpcs.bs   = size(bs_V  , 2);
-numpcs      = min(numpcs.org, numpcs.bs);
+	numpcs.orig = size(orig_V, 2);
+	numpcs.bs   = size(bs_V  , 2);
+	numpcs      = min(numpcs.org, numpcs.bs);
 
-r_tmp  = corr(orig_V, bs_V) ;
-r_sign = sign(r_tmp) ;
-r_tmp  = abs( r_tmp) ; 
+	r_tmp  = corr(orig_V, bs_V) ;
+	r_sign = sign(r_tmp) ;
+	r_tmp  = abs( r_tmp) ; 
 
-for pc = 1:numpcs
+	for pc = 1:numpcs
 
-	[ii, jj] = find(r_tmp == max(r_tmp(:))) ;
+		[ii, jj] = find(r_tmp == max(r_tmp(:))) ;
 
-	pc_ind( ii) = jj;
-	pc_sign(ii) = r_sign(ii, jj) ;
-	r_tmp(ii,:) = -1;
-	r_tmp(:,jj) = -1;
+		pc_ind( ii) = jj;
+		pc_sign(ii) = r_sign(ii, jj) ;
+		r_tmp(ii,:) = -1;
+		r_tmp(:,jj) = -1;
+
+	end
 
 end
-
