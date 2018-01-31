98e581e0db3bf33899587cf490c41e931026840d
BalmungSama <jbryaneusebio@gmail.com>
2018-01-31 16:23:36 -0500

    lowered execution time

--- pca_on_fmri.sh
+++ pca_on_fmri.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -c 4            # Number of CPUS requested. If omitted, the default is 1 CPU.
 #SBATCH --mem=10240     # Memory requested in megabytes. If omitted, the default is 1024 MB.
-#SBATCH -t 0-20:0:0      # How long will your job run for? If omitted, the default is 3 hours.
+#SBATCH -t 0-0:30:0      # How long will your job run for? If omitted, the default is 3 hours.
 
 ## grab user input 
 
